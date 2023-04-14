97febdd9b11e3eec87127ed7769cfb06c5481025
Andrés Yáñez <andres.escobar.aplicasoftware@gmail.com>
2023-04-14 13:28:18 -0500

    Aplicando Laravel Lang en el formulario de inicio de sesión

---
+++ lang/es/button.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'login' => 'Iniciar Sesión',
+    'logout' => 'Cerrar Sesión',
+    'forgot-password' => '¿Olvidaste tu contraseña?'
+];
--- resources/views/pages/auth/login.blade.php
+++ resources/views/pages/auth/login.blade.php
@@ -31,26 +31,25 @@
         <div class="login_wrapper">
             <div class="animate form login_form">
                 <section class="login_content">
+                    <img src="{{ asset('assets/images/logo-fesc') }}" alt="" class="img-fluid">
                     <form>
-                        <h1>Login Form</h1>
+                        <p>@lang('title.login')</p>
                         <div>
-                            <input type="text" class="form-control" placeholder="Username" required="" />
+                            <input type="text" class="form-control" placeholder="{{ __('field.email') }}"
+                                required="" />
                         </div>
                         <div>
-                            <input type="password" class="form-control" placeholder="Password" required="" />
+                            <input type="password" class="form-control" placeholder="{{ __('field.password') }}"
+                                required="" />
                         </div>
                         <div>
-                            <a class="btn btn-default submit" href="index.html">Log in</a>
-                            <a class="reset_pass" href="#">Lost your password?</a>
+                            <a class="btn btn-danger submit" href="index.html">@lang('button.login')</a>
+                            <a class="reset_pass" href="#">@lang('button.forgot-password')</a>
                         </div>
 
                         <div class="clearfix"></div>
 
                         <div class="separator">
-                            <p class="change_link">New to site?
-                                <a href="#signup" class="to_register"> Create Account </a>
-                            </p>
-
                             <div class="clearfix"></div>
                             <br />
 
