2022d9939e69ec115bbb4ad031b94ffc0b9adab6
Andrés Yáñez <andres.escobar.aplicasoftware@gmail.com>
2023-04-15 10:30:54 -0500

    Cambiando el font-weight del titulo SIPAES dentro del Sidebar

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -31,11 +31,12 @@
 <body class="nav-md">
     <div class="container body">
         <div class="main_container">
-            <div class="col-md-3 left_col">
+            <div class="col-md-3 left_col menu_fixed">
                 <div class="left_col scroll-view">
                     <div class="navbar nav_title" style="border: 0;">
-                        <a href="#" class="site_title"><i class="fa fa-institution"></i>
-                            <span>@lang('title.login-bottom')!</span></a>
+                        <a href="#" class="site_title">
+                            <span>{!! config('app.name') !!}</span>
+                        </a>
                     </div>
 
                     <div class="clearfix"></div>
